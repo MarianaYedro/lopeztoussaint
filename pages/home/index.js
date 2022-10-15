3aec38540be3785fb8347bea2238f0f74a3e13b6
MarianaYedro <marianayedro@gmail.com>
2022-10-15 20:19:42 -0300

    nav fixed

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,25 +1,11 @@
-import PropTypes from "prop-types";
 import Layout from "../../components/Layout/Layout";
-import Navbar from "../../components/Navbar/Navbar";
-//import SliderCustomers from "../../components/Customers/SliderCustomers";
-//import Section from "../../components/Section/Section";
-//import SectionAbout from "../../components/SectionAbout/SectionAbout";
-//import Form from "../../components/Form/Form";
-//import Footer from "../../components/Footer/Footer";
-//import All from "../../components/Footer/All";
-//import "slick-carousel/slick/slick.css";
-//import "slick-carousel/slick/slick-theme.css";
 import styles from "./index.module.css";
-//import SliderServices from "../../components/Consultoria/SliderServices";
-import ES from "./es.json";
-import Image from "next/image";
-//import RRSS from "../../components/RRSS/RRSS";
+import Navbar from "../../components/Navbar/Navbar";
 
 
 export const Home = ({ data }) => {
-    const {about, services, section} = data;
     return (
-        <>
+        
             <Layout title="LTS | Home">
                 <div className={styles.container}>
                     <Navbar/>
@@ -29,7 +15,7 @@ export const Home = ({ data }) => {
                 
                 
             </Layout>
-        </>
+        
     );
 };
 
